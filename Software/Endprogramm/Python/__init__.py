5d0a00ab8e2d1171fda3db3542067f54627871bc
I8pieunihaspel <43341462+I8pieunihaspel@users.noreply.github.com>
2018-09-18 13:13:01 +0200

    Add files via upload

--- Software/Endprogramm/Python/__init__.py
+++ Software/Endprogramm/Python/__init__.py
@@ -2,5 +2,6 @@ from Sensor import Sensor, LightSensor, DistanceSensor
 if __name__ == "__main__":
     s1 = LightSensor(14, 0)
     s2 = DistanceSensor(4, 0)
-    print(s1.get_location())
-    print(s2.get_location())
+    while True:
+        print(s1.get_location() + " : " + str(s1.get_value()))
+        print(s2.get_location() + " : " + str(s2.get_value()))
