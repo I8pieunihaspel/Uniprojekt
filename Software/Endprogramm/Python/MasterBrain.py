4664909ac5f13803107aa1c3ab4473708b379968
I8pieunihaspel <gitgood@web.de>
2018-09-27 12:16:49 +0200

    added Files
    Manuel

---
+++ Software/Endprogramm/Python/MasterBrain.py
@@ -0,0 +1,10 @@
+class MasterBrain(object):
+    def __init__(self):
+        pass
+    def einparken(self):
+        pass
+    def parklücke_suchen(self):
+        pass
+    def einparken(self, seconds:int):
+        pass
+    
--- Software/Endprogramm/Python/Motortreiber.py
+++ Software/Endprogramm/Python/Motortreiber.py
@@ -2,4 +2,4 @@ class Motortreiber(object):
     def __init__(self):
         pass
     def pwm(self, GPIO, power_level):
-        
+        pass
